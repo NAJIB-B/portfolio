bd4fecdb7068c601f97f5494b53131bf5f3071a0
NAJIB-B <najibajibasu@gmail.com>
2023-03-29 07:56:03 +0100

    first attempt in fixing build added netlify toml file

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 import '../styles/About.css'
 
 const About = () => {
-  const calculate_age = () => {
-    var today = new Date();
-    var birthDate = new Date("1994-10-16");
-    var age_now = today.getFullYear() - birthDate.getFullYear();
-    var m = today.getMonth() - birthDate.getMonth();
-    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-      age_now--;
-    }
-    return age_now;
-  }
+  // const calculate_age = () => {
+  //   var today = new Date();
+  //   var birthDate = new Date("1994-10-16");
+  //   var age_now = today.getFullYear() - birthDate.getFullYear();
+  //   var m = today.getMonth() - birthDate.getMonth();
+  //   if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+  //     age_now--;
+  //   }
+  //   return age_now;
+  // }
   return (
     <div className='about' id="about">
       <h2>Get To Know Me</h2>
